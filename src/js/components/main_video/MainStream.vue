a163f02270c917e4ee7f720bdb6fc4d5d2c38586
Hung Tran <blackingbar27@gmail.com>
2018-07-20 15:33:52 +0700

    add

--- src/js/components/main_video/MainStream.vue
+++ src/js/components/main_video/MainStream.vue
@@ -2,7 +2,32 @@
 <div>
     <div class="video-player-wrap">
         <div class="video-player">
-            <div id="laracasts-video" class="container" data-vimeo-initialized="true"><div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/267261283?speed=1&amp;color=00b1b3&amp;autoplay=1&amp;app_id=122963" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" title="1-Piece-By-Piece-Structure-Resets-and-Flexbox" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" data-ready="true"></iframe></div></div>
+            <div id="laracasts-video" class="container"">
+                <div style="">
+
+                    <video-player  class="video-player-box"
+                                   ref="videoPlayer"
+                                   :options="playerOptions"
+                                   :playsinline="true"
+                                   customEventName="customstatechangedeventname"
+
+                                   @play="onPlayerPlay($event)"
+                                   @pause="onPlayerPause($event)"
+                                   @ended="onPlayerEnded($event)"
+                                   @waiting="onPlayerWaiting($event)"
+                                   @playing="onPlayerPlaying($event)"
+                                   @loadeddata="onPlayerLoadeddata($event)"
+                                   @timeupdate="onPlayerTimeupdate($event)"
+                                   @canplay="onPlayerCanplay($event)"
+                                   @canplaythrough="onPlayerCanplaythrough($event)"
+
+                                   @statechanged="playerStateChanged($event)"
+                                   @ready="playerReadied">
+                    </video-player>
+
+
+                </div>
+            </div>
         </div>
     </div>
     <div class="match-details">
@@ -26,9 +51,76 @@
 </template>
 
 <script>
-    export default {
+  import 'video.js/dist/video-js.css'
 
+  import { videoPlayer } from 'vue-video-player'
+
+  export default {
+
+    components: {
+      videoPlayer
+    },
+
+    data() {
+      return {
+        playerOptions: {
+          // videojs options
+          muted: false,
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: "video/mp4",
+            src: "https://video.xx.fbcdn.net/v/t42.9040-2/10000000_285376475550902_6430911913063874560_n.mp4?_nc_cat=0&efg=eyJybHIiOjE1MDAsInJsYSI6NDA5NiwidmVuY29kZV90YWciOiJzdmVfaGQifQ%3D%3D&rl=1500&vabr=962&oh=48a548e018b9c56df913008d86a9b24b&oe=5B5428F2"
+          }],
+          poster: "https://s3img.vcdn.vn/123phim/2017/04/em-chua-18-14925939804005.jpg",
+        }
+      }
+    },
+
+    computed: {
+      player() {
+        return this.$refs.videoPlayer.player
+      }
+    },
+
+    methods: {
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerPause(player) {
+        // console.log('player pause!', player)
+      },
+      onPlayerEnded(player) {
+        // console.log('player ended!', player)
+      },
+      onPlayerLoadeddata(player) {
+        // console.log('player Loadeddata!', player)
+      },
+      onPlayerWaiting(player) {
+        // console.log('player Waiting!', player)
+      },
+      onPlayerPlaying(player) {
+        // console.log('player Playing!', player)
+      },
+      onPlayerTimeupdate(player) {
+        // console.log('player Timeupdate!', player.currentTime())
+      },
+      onPlayerCanplay(player) {
+        // console.log('player Canplay!', player)
+      },
+      onPlayerCanplaythrough(player) {
+        // console.log('player Canplaythrough!', player)
+      },
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        console.log('player current update state', playerCurrentState)
+      },
+      // player is ready
+      playerReadied(player) {
+        console.log('example 01: the player is readied', player)
+      }
     }
+  }
 </script>
 
 <style>
